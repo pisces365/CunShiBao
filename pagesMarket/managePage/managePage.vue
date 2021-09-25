3a2b94ed696eddb657981f545147287d8a7247eb
pisces <2926335628@qq.com>
2021-09-25 17:10:25 +0800

    9.25 二手yjn

---
+++ pagesMarket/managePage/managePage.vue
@@ -0,0 +1,396 @@
+<template>
+	<view>
+		<view>
+			<view class="index" v-show="ifLoading">
+				<view class="secondHandItems">
+					<navigator url="../../pagesMarket/secondHandDetails/secondHandDetails" class="secondHandItem">
+						<view>
+							<image class="secondHandImage" src="http://p1362.bvimg.com/10465/007027ee5b250fa6.jpg"  style="width: 340rpx;"></image>
+						</view>
+						<view>
+							<text class="goods-name">二手商品名称</text>
+						</view>
+						<view class="">
+							<text class="brand-and-condition">品质</text>
+							<text class="brand-and-condition">|</text>
+							<text class="brand-and-condition">品牌</text>
+						</view>
+						<view class="" style="margin-bottom: 10rpx;">
+							<text class="price-now">￥1288</text>
+							<text class="price-before">￥1500</text>
+							<text class="discount">已减￥375</text>
+						</view>
+					</navigator>
+					<navigator url="../../pagesMarket/secondHandDetails/secondHandDetails" class="secondHandItem">
+						<view>
+							<image class="secondHandImage" src="http://p1362.bvimg.com/10465/007027ee5b250fa6.jpg"  style="width: 340rpx;"></image>
+						</view>
+						<view>
+							<text class="goods-name">二手商品名称</text>
+						</view>
+						<view class="">
+							<text class="brand-and-condition">品质</text>
+							<text class="brand-and-condition">|</text>
+							<text class="brand-and-condition">品牌</text>
+						</view>
+						<view class="" style="margin-bottom: 10rpx;">
+							<text class="price-now">￥1288</text>
+							<text class="price-before">￥1500</text>
+							<text class="discount">已减￥375</text>
+						</view>
+					</navigator>
+				</view>
+				<view class="secondHandItems">
+					<navigator url="../../pagesMarket/secondHandDetails/secondHandDetails" class="secondHandItem">
+						<view>
+							<image class="secondHandImage" src="http://p1362.bvimg.com/10465/007027ee5b250fa6.jpg"  style="width: 340rpx;"></image>
+						</view>
+						<view>
+							<text class="goods-name">二手商品名称</text>
+						</view>
+						<view class="">
+							<text class="brand-and-condition">品质</text>
+							<text class="brand-and-condition">|</text>
+							<text class="brand-and-condition">品牌</text>
+						</view>
+						<view class="" style="margin-bottom: 10rpx;">
+							<text class="price-now">￥1288</text>
+							<text class="price-before">￥1500</text>
+							<text class="discount">已减￥375</text>
+						</view>
+					</navigator>
+					<navigator url="../../pagesMarket/secondHandDetails/secondHandDetails" class="secondHandItem">
+						<view>
+							<image class="secondHandImage" src="http://p1362.bvimg.com/10465/007027ee5b250fa6.jpg"  style="width: 340rpx;"></image>
+						</view>
+						<view>
+							<text class="goods-name">二手商品名称</text>
+						</view>
+						<view class="">
+							<text class="brand-and-condition">品质</text>
+							<text class="brand-and-condition">|</text>
+							<text class="brand-and-condition">品牌</text>
+						</view>
+						<view class="" style="margin-bottom: 10rpx;">
+							<text class="price-now">￥1288</text>
+							<text class="price-before">￥1500</text>
+							<text class="discount">已减￥375</text>
+						</view>
+					</navigator>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				screenWidth: "",
+				title: 'Hello',
+				ifLoading: false,
+				VillageParty: {
+					VillagePartyTitle: [
+						"二手发布", "我想要的", "我要捐赠", "二手管理"
+					],
+					VillagePartyDescripetion: [
+						"发布你的宝贝",
+						"让世界聆听你的新生",
+						"每个宝贝都有用武之地",
+						"管理我的二手商品"
+					],
+					VillagePartyTitleImageUrl: [
+						"http://p1362.bvimg.com/10465/7f4763358d4dada6.png",
+						"http://p1362.bvimg.com/10465/71814c662f242be1.png",
+						"http://p1362.bvimg.com/10465/4cc0018e009ece23.png",
+						"http://p1362.bvimg.com/10465/950715978bdf8348.png"
+					]
+				}
+			}
+		},
+		onLoad() {
+			this.ifLoading = true;
+					uni.getSystemInfo({
+					    success: function (res) {
+					        // console.log(res.screenHeight); //屏幕高度  注意这里获得的高度宽度都是px 需要转换rpx
+					        // console.log(res.windowWidth);  //可使用窗口宽度
+					        // console.log(res.windowHeight); //可使用窗口高度
+					        console.log(res.screenWidth); //屏幕宽度
+							// var rpx=(res.screenHeight * (750 / res.windowWidth))  //将px 转换rpx
+							// console.log(rpx)
+							this.screenWidth = res.screenWidth / 2 + ""; 
+							
+							console.log(this.screenWidth);
+					    }
+					});
+				},
+
+		methods: {
+			toDetails() {
+				uni.navigateTo({
+					url: ("../../pagesA/News?newsID=0")
+				})
+			},
+			goToVillageHead_Mail() {
+				uni.navigateTo({
+					url: '../../pagesA/VillageHead_Email/IWannaWrite/IWannaWrite'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	
+	@import url("../../common/iconfont.css");
+
+	.secondHandItems {
+		display: flex;
+	}
+	.secondHandItem {
+		flex: 1;
+		margin: 10rpx;
+	}
+	.secondHandImage {
+		border-radius: 20rpx;
+	}
+	.price-now {
+		font-size: 32rpx;
+		color: rgb(243, 153, 18);
+	}
+	.price-before {
+		margin-left: 14rpx;
+		color: rgb(243, 153, 18);
+		font-size: 24rpx;
+		text-decoration: line-through;
+	}
+	.discount {
+		margin-left: 14rpx;
+		color: rgb(243, 153, 18);
+		background-color: rgba(243, 153, 18,0.1);
+		font-size: 24rpx;
+		padding: 6rpx;
+	}
+
+	.goods-name {
+		font-weight: bold;
+		margin-right: 14rpx;
+		font-size: 30rpx;
+	}
+	.brand-and-condition {
+		font-size: 24rpx;
+		color: #A6A6A6;
+		margin:0 8rpx;
+	}
+	
+	.topImage {
+		border-radius: 40rpx;
+		margin:30rpx;
+		margin-bottom: 0;
+		display: block;	
+		width: 690rpx;
+	}
+	
+	.top {
+		 box-shadow: 0rpx 10rpx 30rpx #dcdddc;
+		 background-color: white;
+		 z-index: 100; 
+		 /* float: left; */
+		 width: 100%; 
+		 /* position: fixed; */
+	}
+
+	.index {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.index-swiper {
+		height: 360rpx;
+		width: 670rpx;
+		margin: 24rpx 0 16rpx 0;
+		border-radius: 20rpx;
+		overflow: hidden;
+	}
+
+	.index-swiper-image image {
+		height: 360rpx;
+		width: 670rpx;
+	}
+
+	.index-blog {
+		background: white;
+		border-radius: 20rpx;
+		margin: 10rpx 10rpx;
+		margin-bottom: 10rpx;
+		width: 670rpx;
+		height: 120rpx;
+		display: flex;
+		flex-direction: row;
+	}
+
+	.index-blog-left {
+		margin-left: 20rpx;
+		width: 120rpx;
+	}
+
+	.index-blog-left image {
+		width: 118rpx;
+		height: 118rpx;
+	}
+
+	.index-blog-center-line {
+		background: rgba(0, 0, 0, .3);
+		height: 66rpx;
+		width: 4rpx;
+	}
+
+	.index-blog-center {
+		height: 100%;
+		width: 4rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-left: 10rpx;
+	}
+
+	.index-blog-right {
+		margin: 14rpx 16rpx 14rpx 14rpx;
+		margin-left: 20rpx;
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		flex: 1;
+
+	}
+
+	.index-blog-right-top {}
+
+	.index-blog-right-top text {
+		font-size: 26rpx;
+		color: #3f3f3f;
+	}
+
+	.index-blog-right-bottom {
+		position: absolute;
+		top: 40rpx;
+	}
+
+	.index-blog-right-bottom text {
+		font-size: 22rpx;
+		color: #B8B9B8;
+
+
+	}
+
+	.index-blog-right-icon {
+		position: absolute;
+		top: 20rpx;
+		right: 0rpx;
+	}
+
+	.index-blog-right-icon text {
+		color: #00aa7f;
+		font-size: 44rpx;
+	}
+
+	.index-VillageParty {
+		width: 670rpx;
+		/* height: 500rpx; */
+		margin: 10rpx;
+		/* background: green; */
+	}
+
+	.index-ServieceToPeople {
+		margin-top: 10rpx;
+		width: 670rpx;
+		height: 480;
+	}
+
+	.index-VillageParty-title,
+	.index-ServieceToPeople-title,
+	.index-polling-title {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.index-VillageParty-title-image image,
+	.index-polling-title-image image {
+		height: 100rpx;
+		width: 100rpx;
+	}
+
+	.index-VillageParty-title text {
+		margin-left: 14rpx;
+		font-size: 34rpx;
+	}
+
+	.index-VillageParty-content-onePart {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 20rpx;
+
+	}
+
+	.index-VillageParty-content-onePart-left {
+		width: 300rpx;
+		height: 114rpx;
+		background: white;
+		border-radius: 20rpx;
+		margin: 0 16rpx;
+		position: relative;
+	}
+	.index-VillageParty-content-onePart-noShow {
+		width: 300rpx;
+		height: 114rpx;
+		/* background: white; */
+		border-radius: 20rpx;
+		margin: 0 16rpx;
+		position: relative;
+	}
+
+	.funcion-top {
+		padding: 10rpx 0 4rpx 20rpx;
+	}
+
+	.funcion-top text {
+		font-size: 30rpx;
+		color: #3f3f3f;
+	}
+
+	.funcion-bottom {
+		position: absolute;
+		top: 44rpx;
+		padding: 0 0 4rpx 20rpx;
+	}
+
+	.funcion-bottom text {
+		font-size: 20rpx;
+		color: #B3B4B3;
+	}
+
+	.function-image {
+		position: absolute;
+		height: 114rpx;
+		width: 114rpx;
+		top: 0;
+		right: 8rpx;
+	}
+
+	.function-image image {
+		width: 100%;
+		height: 100%;
+	}
+
+	.index-polling {
+		width: 670rpx;
+		height: 240rpx;
+	}
+</style>
--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -876,7 +876,7 @@ function initData(vueOptions, context) {
     try {
       data = data.call(context); // 支持 Vue.prototype 上挂的数据
     } catch (e) {
-      if (Object({"NODE_ENV":"development","VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.warn('根据 Vue 的 data 函数初始化小程序 data 失败，请尽量确保 data 函数中不访问 vm 对象，否则可能影响首次数据渲染速度。', data);
       }
     }
@@ -2145,7 +2145,7 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.baseUrl = 
 
 /***/ }),
 
-/***/ 181:
+/***/ 183:
 /*!*********************************************************************************************!*\
   !*** D:/uni-appWorkSpace/CunShiBao/CunShiBao/pagesA/rent/host_qualification/citys/citys.js ***!
   \*********************************************************************************************/
@@ -8706,7 +8706,7 @@ function type(obj) {
 
 function flushCallbacks$1(vm) {
     if (vm.__next_tick_callbacks && vm.__next_tick_callbacks.length) {
-        if (Object({"NODE_ENV":"development","VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
+        if (Object({"VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:flushCallbacks[' + vm.__next_tick_callbacks.length + ']');
@@ -8727,14 +8727,14 @@ function nextTick$1(vm, cb) {
     //1.nextTick 之前 已 setData 且 setData 还未回调完成
     //2.nextTick 之前存在 render watcher
     if (!vm.__next_tick_pending && !hasRenderWatcher(vm)) {
-        if(Object({"NODE_ENV":"development","VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:nextVueTick');
         }
         return nextTick(cb, vm)
     }else{
-        if(Object({"NODE_ENV":"development","VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance$1 = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance$1.is || mpInstance$1.route) + '][' + vm._uid +
                 ']:nextMPTick');
@@ -8820,7 +8820,7 @@ var patch = function(oldVnode, vnode) {
     });
     var diffData = this.$shouldDiffData === false ? data : diff(data, mpData);
     if (Object.keys(diffData).length) {
-      if (Object({"NODE_ENV":"development","VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"VUE_APP_NAME":"CunShiBao","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
           ']差量更新',
           JSON.stringify(diffData));
@@ -9228,7 +9228,7 @@ internalMixin(Vue);
 
 /***/ }),
 
-/***/ 228:
+/***/ 230:
 /*!***********************************************************!*\
   !*** D:/uni-appWorkSpace/CunShiBao/CunShiBao/amap-uni.js ***!
   \***********************************************************/
@@ -9671,7 +9671,38 @@ AMapWX.prototype.getWxLocation = function (a, b) {
 
 /***/ }),
 
-/***/ 293:
+/***/ 3:
+/*!***********************************!*\
+  !*** (webpack)/buildin/global.js ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || new Function("return this")();
+} catch (e) {
+	// This works if the window reference is available
+	if (typeof window === "object") g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ }),
+
+/***/ 365:
 /*!************************************************************************************!*\
   !*** D:/uni-appWorkSpace/CunShiBao/CunShiBao/components/city-select/citySelect.js ***!
   \************************************************************************************/
@@ -9718,38 +9749,18 @@ citySelect;exports.default = _default;
 
 /***/ }),
 
-/***/ 3:
-/*!***********************************!*\
-  !*** (webpack)/buildin/global.js ***!
-  \***********************************/
+/***/ 4:
+/*!**********************************************************!*\
+  !*** D:/uni-appWorkSpace/CunShiBao/CunShiBao/pages.json ***!
+  \**********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var g;
-
-// This works in non-strict mode
-g = (function() {
-	return this;
-})();
-
-try {
-	// This works if eval is allowed (see CSP)
-	g = g || new Function("return this")();
-} catch (e) {
-	// This works if the window reference is available
-	if (typeof window === "object") g = window;
-}
-
-// g can still be undefined, but nothing to do about it...
-// We return undefined, instead of nothing here, so it's
-// easier to handle this case. if(!global) { ...}
-
-module.exports = g;
 
 
 /***/ }),
 
-/***/ 329:
+/***/ 401:
 /*!***************************************************************************************************!*\
   !*** D:/uni-appWorkSpace/CunShiBao/CunShiBao/uni_modules/uni-icons/components/uni-icons/icons.js ***!
   \***************************************************************************************************/
@@ -9888,17 +9899,6 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
   "cloud-download-filled": "\uE8E9",
   "headphones": "\uE8BF",
   "shop": "\uE609" };exports.default = _default;
-
-/***/ }),
-
-/***/ 4:
-/*!**********************************************************!*\
-  !*** D:/uni-appWorkSpace/CunShiBao/CunShiBao/pages.json ***!
-  \**********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-
 
 /***/ })
 
