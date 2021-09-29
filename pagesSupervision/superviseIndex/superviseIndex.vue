dbf2809159b8aeefb04f0f5d98dee7854254a7e4
XiaoFeng <1244762145@qq.com>
2021-09-29 23:29:12 +0800

    村务监督首页

---
+++ pagesSupervision/superviseIndex/superviseIndex.vue
@@ -0,0 +1,59 @@
+<template>
+	<view>
+		<view class="supervise">
+			<view class="supervise-left">
+				<view class="supervise-feedback">
+					<view class="title">
+						
+					</view>
+					<view class="enter">
+						<text class="iconfont "></text>
+					</view>
+					<view class="enterText">
+						进入
+					</view>
+					<view class="image">
+						<image src="http://p1362.bvimg.com/10465/43739d9402b28a6a.jpg" mode=""></image>
+					</view>
+				</view>
+				<view class="supervise-environment">
+					
+				</view>
+				<view class="supervise-epidemic">
+					
+				</view>
+			</view>
+			<view class="supervise-right">
+				<view class="supervise-volunteer">
+					
+				</view>
+				<view class="supervise-construction">
+					
+				</view>
+				<view class="supervise-garbage">
+					
+				</view>
+				<view class="supervise-correlation">
+					
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pagesSupervision/pagesSupervision/superviseIndex/superviseIndex.js
@@ -0,0 +1,20 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pagesSupervision/pagesSupervision/superviseIndex/superviseIndex"],{
+
+/***/ 156:
+/*!****************************************************************************************************************************************************************!*\
+  !*** C:/Users/12447/OneDrive/桌面/source/code/uni-app/village-treasure/main.js?{"page":"pagesSupervision%2FpagesSupervision%2FsuperviseIndex%2FsuperviseIndex"} ***!
+  \****************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _superviseIndex = _interopRequireDefault(__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module './pagesSupervision/pagesSupervision/superviseIndex/superviseIndex.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_superviseIndex.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ })
+
+},[[156,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../../.sourcemap/mp-weixin/pagesSupervision/pagesSupervision/superviseIndex/superviseIndex.js.map
